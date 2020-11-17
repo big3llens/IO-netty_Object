841853b39f3a962d26e874fdefec78d39ba026e5
Stanis <big3llenc@gmail.ru>
2020-11-17 17:32:05 +0400

    1

--- src/main/java/Netty_Object_Server/MainHendler.java
+++ src/main/java/Netty_Object_Server/MainHendler.java
@@ -3,27 +3,58 @@ package Netty_Object_Server;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class MainHendler extends ChannelInboundHandlerAdapter {
-    private Path rootPath = Path.of("server");
+    private final Path rootPath = Path.of("server1");
     private FileWorker fw = new FileWorker(rootPath);
+    private Path newFile;
+    private FileOutputStream fos;
+    private FileInputStream fis;
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-//        ctx.write(new Parcel((byte) 10, "С подключением"));
-//        System.out.println("Клиент подключился");
+        ctx.writeAndFlush(new Parcel((byte) 10, "С подключением"));
+        System.out.println("Клиент подключился");
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        Parcel getParcel = (Parcel) msg;
-        if (getParcel.getMessageType() == 20){
-
-        }
-        if (getParcel.getMessageType() == 10) {
-            ctx.write(new Parcel((byte) 10, getParcel.getMessage()));
-            System.out.println(getParcel.toString());
+        Parcel parcel = (Parcel) msg;
+        switch (parcel.getMessageType()){
+            case 10:
+                ctx.write(new Parcel((byte) 10, parcel.getMessage()));
+                System.out.println(parcel.toString());
+                break;
+            case 20:
+                if (!Files.exists(Path.of(fw.getCurrentPath().toString() + "/" + parcel.getNameFile()))){
+                    ctx.write(new Parcel((byte)10, "Файла: " + "[" + parcel.getNameFile() + "]" + " в директории: " + "[" + fw.getCurrentPath() + "]" + " нет"));
+                }
+                newFile = Path.of(fw.getCurrentPath().toString() + "/" + parcel.getNameFile());
+                fis = new FileInputStream(newFile.toFile());
+                if (newFile.toFile().length() < 2000000000){
+                    byte[] bytes = new byte[(int)newFile.toFile().length()];
+                    fis.read(bytes, 0, bytes.length);
+                    ctx.write(new Parcel((byte)20, newFile.toFile().getName(), bytes));
+                }
+                break;
+            case 30:
+                if (!Files.exists(Path.of(fw.getCurrentPath() + "/" + parcel.getNameFile()))) {
+                    Files.createFile(Path.of(fw.getCurrentPath() + "/" + parcel.getNameFile()));
+                }
+                newFile = Path.of(fw.getCurrentPath() + "/" +parcel.getNameFile());
+                System.out.println(fw.getCurrentPath());
+                System.out.println(newFile.toString());
+                System.out.println(newFile.toAbsolutePath());
+                fos = new FileOutputStream(newFile.toFile());
+                fos.write(parcel.getFileContents());
+                fos.flush();
+                break;
         }
     }
 
