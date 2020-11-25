1bdc8c613277ee82fa612284f2484988be2d84aa
Stanis <big3llenc@gmail.ru>
2020-11-25 11:19:24 +0300

    Final version

--- Client/src/main/java/Client.java
+++ Client/src/main/java/Client.java
@@ -4,6 +4,8 @@ import io.netty.handler.codec.serialization.ObjectEncoderOutputStream;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.*;
@@ -25,14 +27,14 @@ public class Client extends JFrame {
         objectOut = new ObjectEncoderOutputStream(new BufferedOutputStream(socket.getOutputStream()));
         prepareGui();
         new Thread(() -> {
-            while (true){
+            while (true) {
                 try {
                     Parcel getParcel = (Parcel) objectIn.readObject();
-                    if (getParcel.getMessageType() == 10){
+                    if (getParcel.getMessageType() == 10) {
                         chatArea.append(getParcel.getMessage() + "\n");
                     }
-                    if (getParcel.getMessageType() == 20){
-                        if (!Files.exists(Path.of(rootPath.toString() + "/" + getParcel.getNameFile()))){
+                    if (getParcel.getMessageType() == 20) {
+                        if (!Files.exists(Path.of(rootPath.toString() + "/" + getParcel.getNameFile()))) {
                             Files.createFile(Path.of(rootPath.toString() + "/" + getParcel.getNameFile()));
                         }
                         Path newFile = Path.of(rootPath.toString() + "/" + getParcel.getNameFile());
@@ -59,8 +61,8 @@ public class Client extends JFrame {
         }
     }
 
-    private void sendFile(String filename){
-        if (!Files.exists(Path.of(rootPath.toString() + "/" + filename))){
+    private void sendFile(String filename) {
+        if (!Files.exists(Path.of(rootPath.toString() + "/" + filename))) {
             chatArea.append("Такого файла не существует\n");
         }
         File file = new File(rootPath.toString() + "/" + filename);
@@ -76,20 +78,20 @@ public class Client extends JFrame {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println((int)file.length());
+        System.out.println((int) file.length());
         int read = 0;
-        if (file.length() < 2000000000){
-            byte[] bytes = new byte[(int)file.length()];
+        if (file.length() < 2000000000) {
+            byte[] bytes = new byte[(int) file.length()];
             System.out.println(file.length());
             try {
-                bytes = fin.readNBytes((int)file.length());
+                bytes = fin.readNBytes((int) file.length());
             } catch (IOException e) {
                 e.printStackTrace();
             }
 //            fin.read(bytes, 0, bytes.length);
 //            System.out.println(new Parcel((byte)30, file.getName(), bytes).toString());
             try {
-                objectOut.writeObject(new Parcel((byte)30, file.getName(), bytes));
+                objectOut.writeObject(new Parcel((byte) 30, file.getName(), bytes));
                 objectOut.flush();
                 text.setText("");
                 text.grabFocus();
@@ -101,7 +103,7 @@ public class Client extends JFrame {
 
     private void getFile(String fileName) {
         try {
-            objectOut.writeObject(new Parcel((byte)20, "", fileName));
+            objectOut.writeObject(new Parcel((byte) 20, "", fileName));
             objectOut.flush();
             text.setText("");
             text.grabFocus();
@@ -115,7 +117,7 @@ public class Client extends JFrame {
     }
 
     public void prepareGui() {
-        setBounds(600, 300, 500, 500);
+        setBounds(800, 400, 500, 500);
         setTitle("Клиент");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -127,37 +129,49 @@ public class Client extends JFrame {
 
         // Нижняя панель с полем для ввода сообщений и кнопкой отправки сообщений
         JPanel bottomPanel = new JPanel(new BorderLayout());
-        JButton sendMsg = new JButton("Отправить");
-        bottomPanel.add(sendMsg, BorderLayout.EAST);
+//        JButton sendMsg = new JButton("Отправить");
+//        bottomPanel.add(sendMsg, BorderLayout.EAST);
         text = new JTextField();
+        Font f = new Font("TimesRoman", Font.PLAIN, 14);
+        chatArea.setFont(f);
         add(bottomPanel, BorderLayout.SOUTH);
         bottomPanel.add(text, BorderLayout.CENTER);
 
-        sendMsg.addActionListener(a -> {
+        text.addActionListener(a -> {
             String[] cmd = text.getText().split(" ");
-            switch (cmd[0]){
+            switch (cmd[0]) {
                 case "upload":
-                    sendFile(cmd[1]);break;
+                    sendFile(cmd[1]);
+                    break;
                 case "download":
-                    getFile(cmd[1]);break;
+                    getFile(cmd[1]);
+                    break;
                 case "/auth":
-                    sendMessage((byte)9, cmd[1] + " " + cmd[2]); break;
+                    sendMessage((byte) 9, cmd[1] + " " + cmd[2]);
+                    break;
                 case "ls":
-                    sendMessage((byte)11, ""); break;
+                    sendMessage((byte) 11, "");
+                    break;
                 case "cd":
-                    sendMessage((byte)12, cmd[1]); break;
+                    sendMessage((byte) 12, cmd[1]);
+                    break;
                 case "mkdir":
-                    sendMessage((byte)13, cmd[1]);break;
+                    sendMessage((byte) 13, cmd[1]);
+                    break;
                 case "touch":
-                    sendMessage((byte)14, cmd[1]);break;
+                    sendMessage((byte) 14, cmd[1]);
+                    break;
                 case "rm":
-                    sendMessage((byte)15, cmd[1]);break;
+                    sendMessage((byte) 15, cmd[1]);
+                    break;
                 case "cope":
-                    sendMessage((byte)16, cmd[1]);break;
+                    sendMessage((byte) 16, cmd[1]);
+                    break;
                 case "cat":
-                    sendMessage((byte)17, cmd[1]);break;
+                    sendMessage((byte) 17, cmd[1]);
+                    break;
                 default:
-                    sendMessage((byte)10, text.getText());
+                    sendMessage((byte) 10, text.getText());
 
             }
         });
@@ -166,10 +180,12 @@ public class Client extends JFrame {
             @Override
             public void windowClosing(WindowEvent e) {
                 super.windowClosed(e);
-                sendMessage((byte)10, "exit");
+                sendMessage((byte) 10, "exit");
             }
         });
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         setVisible(true);
     }
 
